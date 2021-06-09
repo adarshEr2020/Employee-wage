f10f00395315690cbd4034c00caac91c5ae22e7e
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-09 10:25:43 +0530

    check employee wage till condition

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,20 +1,22 @@
 package empwage;
 
 public class EmployeeWage {
-
 	//Constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
-
+	public static final int MAX_HRS_IN_MONTH = 100;
+	
 	public static void main(String args[])
 	{
 		// Variables
-		int empHrs = 0, empWage = 0, totalEmpWage =0;
+		int totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays =0;
 		// Computation
-		for(int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 		{
+			int empHrs = 0;
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck)
 			{
@@ -27,7 +29,8 @@ public class EmployeeWage {
 			default:
 				empHrs = 0;
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpHrs = empHrs;
+			int empWage = empHrs * EMP_RATE_PER_HOUR;
 			totalEmpWage += empWage;
 			System.out.println("Employee Daily is Wage :" + empWage);
 		}
